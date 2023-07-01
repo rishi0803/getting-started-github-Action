de223143c3dca3614f479ee8c4512627cece3bb7
rishi0803 <128279431+rishi0803@users.noreply.github.com>
2023-07-01 12:40:55 +0530

    Create github-action-script.sh

---
+++ github-action-script.sh
@@ -0,0 +1,9 @@
+ echo what is in this directory?
+            ls -a
+            echo is there a java?
+            java --version
+            echo Is there any build tool?
+            mvn --version
+            gradle --version
+            echo who is running this script?
+            whoami
